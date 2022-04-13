1c6e83f2dfad2ba7e636165551af28ae93d3d5ec
Yudai-creator <perezheber73@gmail.com>
2022-04-13 15:19:40 -0400

    global state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import Header from "./components/HeaderApp";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState(
+        [
+    {
+        id: 1,
+        text: 'Photo shoot',
+        day: 'May 1st at 2:30pm',
+        reminder: false
+    },
+    {
+        id: 2,
+        text: 'Shopping',
+        day: 'April 15th at 8:30am',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Twitter Space',
+        day: 'April 16th at 9:30am',
+        reminder: true
+    }
+]
+    )
   return (
     <div className="container">
       <Header title="Cool task tracker"/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
